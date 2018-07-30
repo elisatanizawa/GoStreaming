b088a66ed3a321f685f6f7ecd79d98a4e4c6061c
HenryVanAngel <41913486+HenryVanAngel@users.noreply.github.com>
2018-07-30 16:13:44 -0300

    Add files via upload
    Form com ip valido agora

---
+++ GAMA-SEMANA2/js/firebase.js
@@ -0,0 +1,55 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBts4x0s8sF7Mrae6mi-riVrhxZdBbCyrw",
+    authDomain: "gostreaming-gama.firebaseapp.com",
+    databaseURL: "https://gostreaming-gama.firebaseio.com",
+    projectId: "gostreaming-gama",
+    storageBucket: "gostreaming-gama.appspot.com",
+    messagingSenderId: "498080195977"
+  };
+  firebase.initializeApp(config);
+  
+  //reference messa collection
+  var messagesRef = firebase.database().ref("messages");
+
+
+document.getElementById("form-cadastro").addEventListener("submit", cadastroForm);
+
+function cadastroForm(c){
+	c.preventDefault();
+
+	//Get values
+
+	var nome = getInputVal("nome");
+	var email = getInputVal("email");
+	var hora = getData();
+	var ip = getIp();
+
+	//salvar dados
+	
+	saveDados(nome,email,hora,ip);
+
+	//limpa form
+
+	document.querySelector("#form-cadastro").reset();
+}
+
+
+
+//funçao para pegar form values
+function getInputVal(id){
+	return document.getElementById(id).value;
+}
+//salvar dados no firebase
+
+function saveDados(nome,email,hora,ip){
+	var newDados = messagesRef.push();
+	newDados.set({
+		nome: nome,
+		email: email,
+		hora: getData(),
+		ip: getIp()
+
+
+	});
+}
